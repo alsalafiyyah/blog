f8b26e784b6eb015a8e1902729479ec4771c6f36
Al-Salafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2026-08-31 01:38:07 +0000

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
-  site: 'https://alsalafiyyah.github.io/blog',
+  site: 'https://alsalafiyyah.github.io',
   base: '/blog', 
 });
