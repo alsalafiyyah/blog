5acbf8b881fde6909286d88af37a81c542710537
Al-Salafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2026-08-31 05:00:23 +0000

    Update

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,9 @@
 export const SITE_TITLE = "Fawaid";
 export const SITE_DESCRIPTION = "Sayings of the Salaf and Scholars of the Sunnah";
 export const SITE_URL = "https://alsalafiyyah.github.io/blog/";
+
+// menu items
+export const NAV_ITEMS = [
+  { label: "Index", href: "/blog/" },
+  { label: "Fatwas", href: "/fatwas/" },
+];
