03bada21848a6f874ecc23874114704ae563bb94
Al-Salafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2026-08-31 04:47:17 +0000

    Update

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "Fawaid";
 export const SITE_DESCRIPTION = "Sayings of the Salaf and Scholars of the Sunnah";
+export const SITE_URL = "https://alsalafiyyah.github.io/blog/";
