3eb7d50eb9437860625b53cc9d1288205fb010f7
Al-Salafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2026-08-31 02:47:07 +0000

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
   // Replace with your actual domain or GitHub Pages URL
   site: 'https://alsalafiyyah.github.io',
-  // Replace '<YOUR-REPOSITORY-NAME>' with your actual repo name (omit if it's a username.github.io root repo)
-  base: '/blogmkdir -p .github/workflows',
+  base: '/blog',
 });
