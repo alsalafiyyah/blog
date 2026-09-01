7beb5adee273cbda13992b2d2ccc7bee17b83674
Al-Salafiyyah <50047708+alsalafiyyah@users.noreply.github.com>
2026-09-01 05:58:30 +0000

    Update

---
+++ src/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="astro/client" />
+
+declare module '*.jfif' {
+  const src: string;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: string;
+  export default src;
+}
